f4ccae812de09c6676d47b9dd5d4ed80b688bab7
devjaewon <devjaewon21@gmail.com>
2024-02-04 21:42:14 +0900

    feat(config): nest/config 서비스 사용

--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService as NestConfigService } from '@nestjs/config';
 import SMTPTransport from 'nodemailer/lib/smtp-transport';
 
 @Injectable()
 export class ConfigService {
+  constructor(private env: NestConfigService = new NestConfigService()) {}
+
   getSenderEmailAddress(): string {
-    return `${process.env.NAVER_USER}@naver.com`;
+    return `${this.env.get('NAVER_USER')}@naver.com`;
   }
 
   createEmailTransportConfig(): SMTPTransport.Options {
@@ -13,8 +16,8 @@ export class ConfigService {
       host: 'smtp.naver.com',
       port: 465,
       auth: {
-        user: process.env.NAVER_USER,
-        pass: decodeURIComponent(process.env.NAVER_PASSWORD),
+        user: this.env.get('NAVER_USER'),
+        pass: decodeURIComponent(this.env.get('NAVER_PASSWORD')),
       },
     };
   }
